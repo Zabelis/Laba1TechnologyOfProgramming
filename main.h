146a194f7d16085951d523385d5f685002b101aa
Zabelis <meller.artem@mail.ru>
2023-10-07 20:06:03 +0300

    Коммит 1

---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by melle on 07.10.2023.
+//
+
+#ifndef LABA1TECHNOLOGYOFPROGRAMMING_MAIN_H
+#define LABA1TECHNOLOGYOFPROGRAMMING_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //LABA1TECHNOLOGYOFPROGRAMMING_MAIN_H
